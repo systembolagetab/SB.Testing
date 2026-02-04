2810dcfd1a733b9777320fdd2cfc4d81e674d416
Arvid Nyden <arvid.nyden@systembolaget.se>
2026-02-04 16:30:51 +0100

    Revert "adding unsecure code"
    This reverts commit db9d0251437e2acc31b1ffbeed685b66134d2644.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-// Insecure: Hardcoded credentials
-string username = "admin";
-string password = "P@ssw0rd123";
-Console.WriteLine($"Logging in as {username} with password {password}");
-
-// Insecure: Use of weak hash algorithm (MD5)
-using System.Security.Cryptography;
-using System.Text;
-string data = "SensitiveData";
-using (MD5 md5 = MD5.Create())
-{
-    byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(data));
-    Console.WriteLine($"MD5 hash: {BitConverter.ToString(hash).Replace("-", "")}");
-}
+//hasdhhasdh
